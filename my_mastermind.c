8fa8114391a1086a604166b5d050c82ff4a97c0c
warren fung <fung_w@qwasar.io>
2022-09-17 21:06:24 +0000

    working on read

--- my_mastermind.c
+++ my_mastermind.c
@@ -19,8 +19,10 @@
 // It's consider as a normal execution.
 
 #include <stdio.h>  //printf
-#include <unistd.h> //read?
-#include <stdlib.h> //for atoi
+#include <unistd.h> //read
+#include <stdlib.h> //for atoi rand() srand()
+#include <time.h>   //time()
+// #include <cstddef>   ???
 
 void initial_message(void){
     printf("Will you find the secret code?\nPlease enter a valid guess\n---\n");
@@ -43,6 +45,8 @@ void current_progress(int correct, int wrong){
 }
 
 int read_user_input(void){
+
+
     //read stdin
         //if length != 4
             //fail
@@ -52,12 +56,33 @@ int read_user_input(void){
             //return -2
 }
 
-void start_game(char* code, int attempts){
-
+char* generate_4_random_numbers(){
+    char *tmp = (char*)malloc(sizeof(char) * 5);
+    tmp[4] = '\0';
+    time_t t;
+    //  initialize random number generator?
+    srand((unsigned) time(&t));
+    for (int i = 0; i < 4; i += 1) {
+        //  create random number 0 - 9 and cast to char
+        tmp[i] = (char)((rand() % 10) + 48);
+    }
+    return tmp;
 }
 
 int my_str_compare(char* string1, char* string2){
-
+    int i = 0;
+    int j = 0;
+    while (string1[i] != '\0' && string2[j] != '\0') {
+        if (string1[i] != string2[j])
+            //  characters don't match
+            return -1;
+        i += 1;
+        j += 1;
+    }
+    if (i != j)
+        //  string length doesn't match
+        return -2;
+    return 0;
 }
 
 int check_attempt_value(char* num_str){
@@ -65,12 +90,12 @@ int check_attempt_value(char* num_str){
     //  not a number or negative number
     if (value <= 0)
         return -1;
-    return 0;
+    return value;
 }
 
 int check_code_values(char* num_str){
     for(int i = 0; num_str[i] != '\0'; i += 1) {
-        int value = atoi(num_str[i]);
+        int value = atoi(&num_str[i]);
         //  checks value 1 - 9
         if (!(value > 0 && value <= 9)) {
             //  checks value 0
@@ -81,17 +106,47 @@ int check_code_values(char* num_str){
     return 0;
 }
 
-int parse_user_parameters(char* user_parameters){
-    if (my_str_compare(user_parameters, "-c") == 0)
-        return 1;
-    else if (my_str_compare(user_parameters, "-t") == 0)
-        return 2;
+int check_user_input(char* code, char* attempt){
+    if (check_code_values(code) == -1)
+        //  code fails
+        return -1;
+    if (check_attempt_value(attempt) == -1)
+        //  attempts fail
+        return -2;
+    return 0;    
+}
+
+//  not sure if needed?
+//  checks arg 3 and 5 for flag string i.e. "-c" for 3, "-t" for 5
+int parse_user_parameters(char* user_parameters, char* flag){
+    int result = my_str_compare(user_parameters, flag);
+    if (result < 0)
+        return -1;
+    return result;
+}
+
+int start_game(char* code, char* attempts, int ac){
+    initial_message();
 }
 
 int main(int ac, char** av){
+    if (ac == 1) {
+        char test[6];
+        // int x = read(0, test, sizeof(test));
+        int limit = 6;
+        int x;
+        while ((x = read(0, test, sizeof(test)) > 0)){
+            x = read(0, test, sizeof(test));
+            write(1, test, x);
+            test[5] = '\0';
+            // limit -= 1;
+        }
+        test[5] = '\0';
+        printf("hmm input is\n[%s]\n", test);
+    }
     printf("total args [%i]\n", ac);
     printf("test [%s]\n", av[0]);
-    switch(ac){
+    switch(ac){     //parse?
         // //  program
         // case 0:
         // printf("[%s]", av[0]);
@@ -99,7 +154,8 @@ int main(int ac, char** av){
 
         //  arg 1 - random code
         case 1:
-        printf("[%s]", av[0]);
+        printf("[%s]\n", av[0]);
+        start_game(NULL, NULL, 0);
         break;
 
         case 2: //random code
