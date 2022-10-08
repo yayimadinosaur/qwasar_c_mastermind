a3bd0f025b5d6ed590f6092c3e86ac280be4e729
warren fung <fung_w@qwasar.io>
2022-10-08 18:59:07 +0000

    neeva presentation research

---
+++ neeva.py
@@ -0,0 +1,67 @@
+#   site
+#       neeva.com
+#   summary
+#       founders
+#           Sridhar Ramaswamy ex-SVP of Ads at Google
+#           Vivek Raghunathan ex-VP of Monetization at YouTube
+#       ad-free secure search engine
+#       customize your browser experience
+#           you select your ads - news + shopping (more to come)
+#       data encrypted at rest (stored) and in-transit (data traveling)
+#       anonymous searching
+#       browser data auto-deletes in 90 days
+#       your data is never shared or sold
+#           only used for their own research to improve your search results
+#       tells you exactly what ad/tracker was blocked and from where
+#       
+#   platforms
+#       browser extension
+#           chrome, firefox, safari, brave, edge
+
+#       mobile app
+#           iOS + android
+#   membership
+#       basic - free / can always upgrade to premium at any time
+#       
+#       premium - paid
+#           privacy protection + exclusive benefits (not mentioned)
+#           lastpass premium bundle + bitdefender premium VPN
+#   usage / features
+#       can connect third party apps i.e. 
+#           google, oficce365, drop, slack, github
+#           confluence, jira, box, notion,figma
+#       search through all your connected apps as well
+#       associate limited information about you + your accounts
+#           uses that information to better your search experience
+#       their own private forum / community based searches built-in
+#           reddit / stackoverflow / stackexchange / hackernews
+#       can set as default browser - follow guide
+#       book unlimited video call sessions w/ customer support
+#       only shows you data you have access to
+#       world's only ad-free search engine
+#           other private search engines are ad-supported
+#       cookie cutter - eliminate cookie consent pop-ups
+#   staff
+#       a lot of people from Google + Google related companies
+#       experienced investors + management level in tech industry
+#           sequoia, greylock partners, 
+#   financial model / how they make money
+#       sell premium memberships! - monthly subscription
+#           available in U.S. only right now
+#   cool news
+#       released in Europe, UK, France, Germany in 10/22
+#       open sourced their android browser on 09/22
+#       open sourced iOS browser on 03/22
+#   jobs/careers
+#       mountain view, ca, austin, texas
+#       select remote or in-person
+#       tech
+#           ML/NLP - spark, kubernetes
+#           mobile - kotlin (android), swift (iOS)
+#           go, python
+#           browser - react
+#   interview
+#       leetcode easy + medium
+#       take home assignment - a few days to build
+#           4 diff projects to choose from
+#               
